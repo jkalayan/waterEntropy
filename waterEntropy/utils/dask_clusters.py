3027f6d9cadbfb0ccdaeff430f80804f6b39b968
James Gebbie-Rayet <james.gebbie@stfc.ac.uk>
2026-01-06 17:18:53 +0000

    tweak some hpc params and how they are set and used

--- waterEntropy/utils/dask_clusters.py
+++ waterEntropy/utils/dask_clusters.py
@@ -23,9 +23,10 @@ def slurm_submit_master(args):
         f.write("#SBATCH --job-name=waterentropy-master\n")
         f.write("#SBATCH --nodes=1\n")
         f.write("#SBATCH --ntasks=1\n")
-        f.write("#SBATCH --cpus-per-task=1\n")
+        f.write("#SBATCH --cpus-per-task=2\n")
         f.write(f"#SBATCH --time={args.hpc_walltime}\n")
-        f.write(f"#SBATCH --account={args.hpc_account}\n")
+        if args.hpc_account != "":
+            f.write(f"#SBATCH --account={args.hpc_account}\n")
         f.write(f"#SBATCH --partition={args.hpc_queue}\n")
         if args.hpc_qos != "":
             f.write(f"#SBATCH --qos={args.hpc_qos}\n")
@@ -48,6 +49,8 @@ def slurm_configure_cluster(args):
 
     # Extra directives.
     extra = []
+    if args.hpc_account != "":
+        extra.append(f'--account="{args.hpc_account}"')
     if args.hpc_qos != "":
         extra.append(f'--qos="{args.hpc_qos}"')
     if args.hpc_constraint != "":
@@ -66,7 +69,7 @@ def slurm_configure_cluster(args):
     prologue.append("export SLURM_CPU_FREQ_REQ=2250000")
 
     # Interfaces.
-    hpc_nics = ["ib0", "hsn0", "eth0"]
+    hpc_nics = ["bond0", "ib0", "hsn0", "eth0"]
     interfaces = psutil.net_if_addrs()
     for iface in hpc_nics:
         if iface in interfaces:
@@ -81,7 +84,6 @@ def slurm_configure_cluster(args):
         job_directives_skip=skip,
         job_extra_directives=extra,
         python="srun python",
-        account=args.hpc_account,
         walltime=args.hpc_walltime,
         shebang="#!/bin/bash --login",
         local_directory="$PWD",
